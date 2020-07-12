15bb306c8ce965e87d2de047f7c6a0b793ae8c49
TuMmY20 <63773684+TuMmY20@users.noreply.github.com>
2020-07-12 20:15:33 +0300

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,30 @@
+Server & Core
+l!serverInfo l!userInfo invite system
+ticket system l!ping
+
+Games & Texting
+l!123 l!8ball
+l!imgtext l!gayrate
+l!lovecheck l!embed
+l!ascii l!animegif
+l!pat l!slap
+
+Moderation Commands
+l!ban l!kick l!warn
+l!mute l!purge l!role
+
+
+Bot Commands
+l!uptime l!time l!invite
+
+
+Work & Fun
+l!work l!bal l!pay
+l!givemoney l!fish
+l!deposit l!removemoney
+l!leaderboard
+
+
+Minecraft Commands
+l!skin l!server
+l!achievement
